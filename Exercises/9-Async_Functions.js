fdd8683df87308ff06ef0f65760f8dea183c57b9
Juventino Romero <jromero@NSL-JROMEROs-MacBook-Pro.local>
2018-10-26 22:02:00 -0700

    update async exercise

--- Exercises/9-Async_Functions.js
+++ Exercises/9-Async_Functions.js
@@ -1,33 +1,15 @@
 /*
-1. Write a function called hasMostFollowers, which accepts a variable number of arguments. You should then make an AJAX call to the Github User API (https://developer.github.com/v3/users/#get-a-single-user) to get the name and number of followers of each argument. The function should return a string which displays the username who has the most followers. 
+1. Write a function called getCompletedTodos, which accepts a variable number of arguments. You should then make an AJAX call to the jsonplaceholder API to get the ToDos of with the IDs provided as arguments. The function should return a promise, which when resolved, logs and returns an array with the completed ToDos.
 
-Hint - Try to use Promise.all to solve this and remember that the jQuery AJAX methods ($.getJSON, $.ajax, etc.) return a promise.
-*/
-hasMostFollowers('elie','tigarcia','colt').then(function(data){
-    console.log(data)
-});
-// "Colt has the most followers with 424" 
+Hint - Try to use Promise.all to solve this and remember that the axios.get method returns a promise.
 
-/*
-2. Write a function called starWarsString, which accepts a number. You should then make an AJAX call to the Star Wars API (https://swapi.co/ ) to search for a specific character by the number passed to the function. Your function should return a promise that when resolved will console.log the name of the character.
+Hint - To get the ToDo with ID = 1 you must make a request with axios:
+    axios.get("https://jsonplaceholder.typicode.com/todos/1")
 */
-starWarsString(1).then(function(data){
-    console.log(data)
-})
-// "Luke Skywalker"
+const axios = require('axios');
 
-/*
-Bonus 1 -  Using the data from the previous AJAX call above, make another AJAX request to get the first film that character is featured in and return a promise that when resolved will console.log the name of the character and the film they are featured in 
-*/
-starWarsString(1).then(function(data){
-    console.log(data)
-})
-// "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner"
+function getCompletedTodos(...todosIDs) {
+    
+}
 
-/*
-Bonus 2 -  Using the data from Bonus 1 - make another AJAX call to get the information about the first planet that the film contains. Your function should return a promise that when resolved will console.log the name of the character and the film they are featured in and the name of the planet. 
-*/
-starWarsString(1).then(function(data){
-    console.log(data)
-})
-// "Luke Skywalker is featured in The Empire Strikes Back, directed by Irvin Kershner and it takes place on Hoth"
+getCompletedTodos(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);  // 4, 8, 10, 11, 12, 14, 15
